ee7bd334db371a922692803a17cf7522f50c2fde
Maurizio D'Addona <mauritiusdadd@gmail.com>
2015-04-25 10:57:40 +0200

    Fixes some minor bugs

--- salome-kernel-git/salome.sh
+++ salome-kernel-git/salome.sh
@@ -3,7 +3,9 @@
 export LD_LIBRARY_PATH="/usr/lib/qwt:/usr/lib/paraview:${LD_LIBRARY_PATH}"
 export PYTHONPATH="/usr/lib/paraview/site-packages/vtk:/usr/lib/paraview/site-packages:${PYTHONPATH}"
 
-find "/etc/profile.d/" -name "salome-*.conf" -exec source "{}" \;
+for _conf_file in $(find "/etc/profile.d/" -name "salome-*.sh"); do
+  source "${_conf_file}"
+done
 
 runSalome
 
