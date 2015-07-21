a6f8da9da488ea89cd8bf71b6b389d0ece07cbe3
Maurizio D'Addona <mauritiusdadd@gmail.com>
2015-07-21 17:12:38 +0200

    Updates pkg-check.sh

---
+++ info-database.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+EXPAC_VER_w3m="0.5.3-8"
--- pkg-check.sh
+++ pkg-check.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 #expac -S %v w3m | awk -F - '{print $1}'
 #cower -i w3m-mouse --format "%v"
+
+source info-database.sh
 
 [[ -z $AURDIR ]] && export AURDIR="$(pwd)"
 
@@ -17,10 +19,45 @@ isOutOfDate()
   [[ ${PIPESTATUS[0]} -eq 0 ]] ||  echo "??"
 }
 
+checkExpac()
+{
+  db_var="EXPAC_VER_$1"
+  curver="$(expac -S %v $1)"
+  oldver="${!db_var}"
+  if [[ $? -eq 0 ]]; then
+    if [[ $oldver != $curver ]]; then
+      echo "$oldver -> $curver"
+    else
+      echo "updated"
+    fi
+  else
+    echo "[!]"
+  fi
+}
+
+needsUpdate()
+{
+  case $1 in
+    "w3m-mouse")
+      checkExpac w3m
+      ;;
+    "makedumpfile")
+      ;;
+    "calculix")
+      ;;
+    "calculix-doc")
+      ;;
+    *)
+      echo "??"
+      ;;
+  esac
+}
+
 for x in "$AURDIR"/*/; do
   pkgname="$(basename $x)"
   pkgver="$(getCurrentVersion $pkgname)"
   iood="$(isOutOfDate $pkgname)"
-  echo -e "$iood -- $pkgname $pkgver"
+  needupd="$(needsUpdate $pkgname)"
+  printf "%3s -- %-20s\t(%s)\t[%s]\n" "$iood" "$pkgname" "$pkgver" "$needupd"
 done
 
